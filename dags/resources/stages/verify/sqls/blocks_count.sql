557e826517839de35c10baf7673ef849b0da60fc
Evgeny Medvedev <medvedev1088@gmail.com>
2018-12-09 14:45:24 +0700

    Add verify sqls

---
+++ dags/resources/stages/verify/sqls/blocks_count.sql
@@ -0,0 +1,4 @@
+SELECT IF(
+(SELECT MAX(height) FROM bitcoin_blockchain_raw.blocks) + 1 =
+(SELECT COUNT(*) FROM bitcoin_blockchain_raw.blocks), 1,
+CAST((SELECT 'Total number of blocks except genesis is not equal to last block number {{ds}}') AS INT64))
