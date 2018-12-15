bd8a6561c47fc70989a8c8455df437e245d95127
Evgeny Medvedev <medvedev1088@gmail.com>
2018-12-15 20:42:56 +0700

    Refactor load_dag

---
+++ dags/bitcoin_load_dag.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+
+import logging
+
+from airflow.models import Variable, DAG
+
+from build_load_dag import build_load_dag
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+
+dag = build_load_dag(
+    output_bucket=Variable.get('bitcoin_output_bucket'),
+    destination_dataset_project_id=Variable.get('destination_dataset_project_id'),
+    chain='bitcoin',
+    notification_emails=Variable.get('notification_emails', ''),
+    schedule_interval='30 3 * * *'
+)
