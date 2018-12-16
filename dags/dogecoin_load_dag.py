28817d400672a949da33a0f82cf0386ad3608ae7
Evgeny Medvedev <medvedev1088@gmail.com>
2018-12-16 14:56:36 +0700

    Add dogecoin_load_dag.py

--- dags/dogecoin_load_dag.py
+++ dags/dogecoin_load_dag.py
@@ -1,22 +1,21 @@
 from __future__ import print_function
 
-from datetime import datetime
+import logging
 
 from airflow.models import Variable
 
-from bitcoinetl.build_export_dag import build_export_dag
+from bitcoinetl.build_load_dag import build_load_dag
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
 
 # When searching for DAGs, Airflow will only consider files where the string “airflow” and “DAG” both appear in the
 # contents of the .py file.
-DAG = build_export_dag(
-    dag_id='dogecoin_export_dag',
-    provider_uri=Variable.get('dogecoin_provider_uri'),
+DAG = build_load_dag(
+    dag_id='dogecoin_load_dag',
     output_bucket=Variable.get('dogecoin_output_bucket'),
-    start_date=datetime(2013, 12, 6),
+    destination_dataset_project_id=Variable.get('destination_dataset_project_id'),
     chain='dogecoin',
     notification_emails=Variable.get('notification_emails', ''),
-    schedule_interval='0 4 * * *',
-    export_max_workers=3,
-    export_batch_size=10,
-    max_active_runs=15
+    schedule_interval='30 3 * * *'
 )
