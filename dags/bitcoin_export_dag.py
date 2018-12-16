c3741659d5a9ff3a4be91f4ec72f35ea514a443c
Evgeny Medvedev <medvedev1088@gmail.com>
2018-12-16 21:19:36 +0700

    Parameterize export start date

--- dags/bitcoin_export_dag.py
+++ dags/bitcoin_export_dag.py
@@ -6,13 +6,14 @@ from airflow.models import Variable
 
 from bitcoinetl.build_export_dag import build_export_dag
 
+start_date = Variable.get('bitcoin_export_start_date', '2009-01-03')
 # When searching for DAGs, Airflow will only consider files where the string “airflow” and “DAG” both appear in the
 # contents of the .py file.
 DAG = build_export_dag(
     dag_id='bitcoin_export_dag',
     provider_uri=Variable.get('bitcoin_provider_uri'),
     output_bucket=Variable.get('bitcoin_output_bucket'),
-    start_date=datetime(2018, 12, 14),
+    start_date=datetime.strptime(start_date, '%Y-%m-%d'),
     chain='bitcoin',
     notification_emails=Variable.get('notification_emails', ''),
     schedule_interval='0 3 * * *',
--- dags/dogecoin_export_dag.py
+++ dags/dogecoin_export_dag.py
@@ -6,13 +6,14 @@ from airflow.models import Variable
 
 from bitcoinetl.build_export_dag import build_export_dag
 
+start_date = Variable.get('dogecoin_export_start_date', '2013-12-06')
 # When searching for DAGs, Airflow will only consider files where the string “airflow” and “DAG” both appear in the
 # contents of the .py file.
 DAG = build_export_dag(
     dag_id='dogecoin_export_dag',
     provider_uri=Variable.get('dogecoin_provider_uri'),
     output_bucket=Variable.get('dogecoin_output_bucket'),
-    start_date=datetime(2013, 12, 6),
+    start_date=datetime.strptime(start_date, '%Y-%m-%d'),
     chain='dogecoin',
     notification_emails=Variable.get('notification_emails', ''),
     schedule_interval='0 4 * * *',
