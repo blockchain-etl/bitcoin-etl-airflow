a24251b0ffebaae6355dc0814ce512d5ff88fe81
Evgeny Medvedev <medvedev1088@gmail.com>
2018-12-17 15:34:07 +0700

    Add block_height to transactions

--- dags/resources/stages/enrich/sqls/transactions.sql
+++ dags/resources/stages/enrich/sqls/transactions.sql
@@ -34,6 +34,7 @@ SELECT
     transactions.version,
     transactions.lock_time,
     transactions.block_hash,
+    transactions.block_height,
     TIMESTAMP_SECONDS(transactions.block_time) as block_time,
     array(
       select as struct inputs.index, inputs.script_asm, inputs.coinbase_param,
