37ae85797486b99a4a54ef2cd9e54bf343ce04a5
Evgeny Medvedev <medvedev1088@gmail.com>
2018-12-24 15:34:00 +0700

    Fixes

--- dags/resources/stages/verify/sqls/blocks_count.sql
+++ dags/resources/stages/verify/sqls/blocks_count.sql
@@ -1,4 +1,4 @@
 select if(
-(select max(height) from `{{destination_dataset_project_id}}.{{dataset_name}}.blocks`) + 1 =
+(select max(number) from `{{destination_dataset_project_id}}.{{dataset_name}}.blocks`) + 1 =
 (select count(*) from `{{destination_dataset_project_id}}.{{dataset_name}}.blocks`), 1,
 cast((select 'Total number of blocks except genesis is not equal to last block number {{ds}}') as INT64))
