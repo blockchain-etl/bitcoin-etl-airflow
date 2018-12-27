5755b41987916bb9bd76d051a551e7e60375c5d0
Evgeny Medvedev <medvedev1088@gmail.com>
2018-12-27 19:35:57 +0700

    Update litecoin_export_dag.py

--- dags/litecoin_export_dag.py
+++ dags/litecoin_export_dag.py
@@ -6,16 +6,17 @@ from airflow.models import Variable
 
 from bitcoinetl.build_export_dag import build_export_dag
 
+start_date = Variable.get('litecoin_export_start_date', '2011-10-07')
 # When searching for DAGs, Airflow will only consider files where the string “airflow” and “DAG” both appear in the
 # contents of the .py file.
 DAG = build_export_dag(
     dag_id='litecoin_export_dag',
     provider_uri=Variable.get('litecoin_provider_uri'),
     output_bucket=Variable.get('litecoin_output_bucket'),
-    start_date=datetime(2011, 10, 7),
+    start_date=datetime.strptime(start_date, '%Y-%m-%d'),
     chain='litecoin',
     notification_emails=Variable.get('notification_emails', ''),
     schedule_interval='0 5 * * *',
     export_max_workers=4,
-    export_batch_size=1
+    export_batch_size=4
 )
