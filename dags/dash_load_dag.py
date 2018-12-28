6b0042ea9fda1e6b88964dccf562ab7e5a05c5cc
Evgeny Medvedev <medvedev1088@gmail.com>
2018-12-28 23:52:40 +0700

    Add dash dags

--- dags/dash_load_dag.py
+++ dags/dash_load_dag.py
@@ -12,10 +12,10 @@ logging.getLogger().setLevel(logging.DEBUG)
 # When searching for DAGs, Airflow will only consider files where the string “airflow” and “DAG” both appear in the
 # contents of the .py file.
 DAG = build_load_dag(
-    dag_id='litecoin_load_dag',
-    output_bucket=Variable.get('litecoin_output_bucket'),
+    dag_id='dash_load_dag',
+    output_bucket=Variable.get('dash_output_bucket'),
     destination_dataset_project_id=Variable.get('destination_dataset_project_id'),
-    chain='litecoin',
+    chain='dash',
     notification_emails=Variable.get('notification_emails', ''),
-    schedule_interval='30 5 * * *'
+    schedule_interval='30 6 * * *'
 )
