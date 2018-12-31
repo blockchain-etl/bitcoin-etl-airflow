9e260b1da9dd6ebe1266649ce9fec397d5c9245f
Evgeny Medvedev <medvedev1088@gmail.com>
2018-12-31 21:55:23 +0700

    Add zcash dags

---
+++ dags/zcash_load_dag.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+
+import logging
+
+from airflow.models import Variable
+
+from bitcoinetl.build_load_dag import build_load_dag
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+
+# When searching for DAGs, Airflow will only consider files where the string “airflow” and “DAG” both appear in the
+# contents of the .py file.
+DAG = build_load_dag(
+    dag_id='zcash_load_dag',
+    output_bucket=Variable.get('zcash_output_bucket'),
+    destination_dataset_project_id=Variable.get('destination_dataset_project_id'),
+    chain='zcash',
+    notification_emails=Variable.get('notification_emails', ''),
+    schedule_interval='30 7 * * *'
+)
