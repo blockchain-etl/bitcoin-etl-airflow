5fe544096b8d598b7054ce72e20973b4dde9329f
Evgeny Medvedev <medvedev1088@gmail.com>
2019-04-03 14:06:50 +0700

    Fix blocks_count.sql for verifying streaming dag

--- dags/resources/stages/verify_streaming/sqls/blocks_count.sql
+++ dags/resources/stages/verify_streaming/sqls/blocks_count.sql
@@ -2,7 +2,7 @@ select if(
 (
   select count(*) - (max(number) - min(number) + 1)
   from `{{params.destination_dataset_project_id}}.{{params.dataset_name}}.blocks` as blocks
-  where date(timestamp) = '{{ds}}'
+  where date(timestamp) >= date_add('{{ds}}', INTERVAL -1 DAY)
   and timestamp_month >= date_trunc('{{ds}}', MONTH)
-) between 0 and 2, 1,
+) between -2 and 2, 1,
 cast((select 'There are missing blocks or more than 2 duplicate blocks') as INT64))
