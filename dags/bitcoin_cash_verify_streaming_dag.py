dbe69655615a8d382a42dadf7b461adffbfbadb6
medvedev1088 <medvedev1088@gmail.com>
2020-01-25 15:09:58 +0700

    Increase bitcoin cash verify streaming lag

--- dags/bitcoin_cash_verify_streaming_dag.py
+++ dags/bitcoin_cash_verify_streaming_dag.py
@@ -15,6 +15,6 @@ DAG = build_verify_streaming_dag(
     chain='bitcoin_cash',
     **read_verify_streaming_dag_vars(
         var_prefix='bitcoin_cash_',
-        max_lag_in_minutes=220,
+        max_lag_in_minutes=500,
     )
 )
